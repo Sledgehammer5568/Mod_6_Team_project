19972d8944811dbc4b1501ad726855818df1e39c
camrynE <79714509+cam18777@users.noreply.github.com>
2022-11-13 12:03:45 -0600

    Update Mod_6_Group_Project.py

--- Mod_6_Group_Project.py
+++ Mod_6_Group_Project.py
@@ -1,11 +1,22 @@
-# Emanuel Ramos, Camryn Echevarria, Jose Lopez, and David Serna
-# 11/13/2022
-#
-# Last updated by Emanuel Ramos on, ##/##/####
-# Last updated by Camryn Echevarria on, ##/##/####
-# Last updated by Jose Lopez on, ##/##/####
-# Last updated by David Serna on, ##/##/####
-#
-# Description: a program that runs a guessing game
+# Camryn echevarria
+# 11/12/2022
 
+# guessing random number between 1-10.
 import random
+
+print("Hello")
+
+# explain the function of guessing the number
+
+n = random.randrange(1, 10)
+
+guess = int(input("Guess a number between 1 and 10:"))
+
+# generating random number between 1-10
+# pick a low number
+for number in range(0, 10):
+    number_of_guesses = 1
+
+    if guess < n:
+        print("Please guess higher")
+        guess = int(input("Try Entering again:"))
